a276328118397511a52f69bbfa46cb82bb793c4a
Michael ODonnell <36376324+odonnell31@users.noreply.github.com>
2018-03-10 11:04:59 -0500

    Add EDA and metric feat
    Add exploratory data analysis and r2 score metric funcion

--- Machine_Learning_Code/boston_housing_ml.py
+++ Machine_Learning_Code/boston_housing_ml.py
@@ -11,7 +11,7 @@ from sklearn.cross_validation import ShuffleSplit
 import visuals as vs
 
 # Pretty display for notebooks
-%matplotlib inline
+#%matplotlib inline
 
 # Load the Boston housing dataset
 data = pd.read_csv('housing.csv')
@@ -23,3 +23,41 @@ print "Boston housing dataset has {} data points with {} variables each.".format
 print data.head(3)
 print features.head(3)
 print prices.head(3)
+print "======"
+
+# TODO: Minimum price of the data
+minimum_price = np.amin(prices)
+
+# TODO: Maximum price of the data
+maximum_price = np.amax(prices)
+
+# TODO: Mean price of the data
+mean_price = np.mean(prices)
+
+# TODO: Median price of the data
+median_price = np.median(prices)
+
+# TODO: Standard deviation of prices of the data
+std_price = np.std(prices)
+
+# Show the calculated statistics
+print "Statistics for Boston housing dataset:\n"
+print "Minimum price: ${:,.2f}".format(minimum_price)
+print "Maximum price: ${:,.2f}".format(maximum_price)
+print "Mean price: ${:,.2f}".format(mean_price)
+print "Median price ${:,.2f}".format(median_price)
+print "Standard deviation of prices: ${:,.2f}".format(std_price)
+print "====="
+
+# TODO: Import 'r2_score'
+from sklearn.metrics import r2_score
+
+def performance_metric(y_true, y_predict):
+    """ Calculates and returns the performance score between 
+        true and predicted values based on the metric chosen. """
+    
+    # TODO: Calculate the performance score between 'y_true' and 'y_predict'
+    score = r2_score(y_true, y_predict)
+    
+    # Return the score
+    return score
